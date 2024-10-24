a925069679e16f05f0da001a079a4b0e9d3fe040
KingFadilll <erzha237643@student.smkn1kandeman.sch.id>
2024-10-24 14:01:26 +0700

    selesai

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
+  let namaAtlet = ['Rizki','Ambar', 'Rada', 'Tika', 'Malik', 'Nanang', 'Azizah'];
+  let tinggiBadan = ['150', '155', '148', '165', '170', '168', '166'];
 
-
-
-
-
+  for (i = 0; i < 7; i++){
+    console.log('Atlet '+(namaAtlet[i])+' tinggi badannya '+(tinggiBadan[i])+'cm');
+  }
 
   /// JANGAN UBAH SELAINNYA
 }
-
+runner()
 module.exports = runner
